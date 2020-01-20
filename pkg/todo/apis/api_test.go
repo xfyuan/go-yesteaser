02bd90270baa6f33ff8c53a5ea79b17a69f2cc14
Xiaofeng Yuan <yuan.xiaofeng@gmail.com>
2020-01-20 17:15:58 +0800

    add ginkgo tests for todo apis handler

---
+++ pkg/todo/apis/api_test.go
@@ -0,0 +1,75 @@
+package apis
+
+import (
+	"encoding/json"
+	"github.com/gin-gonic/gin"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/xfyuan/go-yesteaser/pkg/app"
+	"github.com/xfyuan/go-yesteaser/pkg/gspec"
+	"github.com/xfyuan/go-yesteaser/pkg/models"
+	"github.com/xfyuan/go-yesteaser/pkg/todo/daos"
+	"github.com/xfyuan/go-yesteaser/pkg/todo/services"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+)
+
+var _ = Describe("Api", func() {
+	var (
+		r *gin.Engine
+		h TodoHandler
+	)
+
+	BeforeEach(func() {
+		app.DB = gspec.ResetDB()
+		r = gspec.NewRouter()
+		h = TodoHandler{
+			service: services.NewTodoService(daos.NewTodoDao(app.DB)),
+		}
+	})
+
+	Describe("#Show", func() {
+		Context("with record", func() {
+			BeforeEach(func() {
+				app.DB.Create(&models.Todo{
+					Title:       "Golang",
+					Description: "Google's language",
+				})
+			})
+
+			It("should get a todo successfully", func() {
+				r.Handle("GET", "/todos/:id", h.Show)
+				res := httptest.NewRecorder()
+				req, _ := http.NewRequest("GET", "/todos/1", nil)
+				r.ServeHTTP(res, req)
+
+				body, _ := ioutil.ReadAll(res.Body)
+				var expect map[string]interface{}
+				if err := json.Unmarshal(body, &expect); err != nil {
+					panic(err)
+				}
+
+				Expect(res.Code).To(Equal(http.StatusOK))
+				Expect(expect["title"]).To(Equal("Golang"))
+				Expect(expect["description"]).To(Equal("Google's language"))
+
+			})
+		})
+
+
+		Context("without record", func() {
+			It("should not found a todo", func() {
+				r.Handle("GET", "/todos/:id", h.Show)
+				res := httptest.NewRecorder()
+				req, _ := http.NewRequest("GET", "/todos/1", nil)
+				r.ServeHTTP(res, req)
+
+				body, _ := ioutil.ReadAll(res.Body)
+
+				Expect(res.Code).To(Equal(http.StatusNotFound))
+				Expect(body).To(BeEmpty())
+			})
+		})
+	})
+})
--- pkg/todo/routes/todo_route.go
+++ pkg/todo/routes/todo_route.go
@@ -3,12 +3,12 @@ package routes
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
-	_todoApi "github.com/xfyuan/go-yesteaser/pkg/todo/api"
+	_todoApis "github.com/xfyuan/go-yesteaser/pkg/todo/apis"
 	_todoDaos "github.com/xfyuan/go-yesteaser/pkg/todo/daos"
 	_todoServices "github.com/xfyuan/go-yesteaser/pkg/todo/services"
 )
 
 func InitRoutes(db *gorm.DB, r *gin.Engine)  {
 	s := _todoServices.NewTodoService(_todoDaos.NewTodoDao(db))
-	_todoApi.NewTodoHandler(r, s)
+	_todoApis.NewTodoHandler(r, s)
 }
